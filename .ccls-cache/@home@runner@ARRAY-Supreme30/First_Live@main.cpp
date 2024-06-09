5de71b388224b175966faf70aee87c8daa12c733
Hithesh Jain <138424080+HitheshJain2002@users.noreply.github.com>
2024-06-09 03:33:30 +0000

    ohk

--- .ccls-cache/@home@runner@ARRAY-Supreme30/First_Live@main.cpp
+++ .ccls-cache/@home@runner@ARRAY-Supreme30/First_Live@main.cpp
@@ -1,14 +1,85 @@
-#include <iostream>
-using namespace std;
+// // #include <iostream>
+// // #include <algorithm>
 
-int main() {
-  int arr[5];
-  for (int i=0;i<5;i++){
-    cin>>arr[i];
-  }
-  cout<<"The array is: " << endl;
-  for (int i=0;i<5;i++){
-    cout<<arr[i];
-  }
+// // using namespace std;
+// // pair<int,int> pairnum(int arr[], int size,int target){
+// //   pair<int,int>ans=make_pair(-1, -1);
+// //   for(int i=0;i<size;i++){
+// //     for(int j=i+1;j<size;j++){
+      
+// //       if(arr[i]+arr[j]==target){
+// //         ans.first=arr[i];
+// //         ans.second=arr[j];
+// //         return ans;
+// //       }
+// //     }
+// //   }
+// // return ans;
   
-}
+// // }
+
+// // int main() {
+  
+// //   int arr[]={30,20,40,10,50};
+// //   int size=5;
+// //   int target=50;
+// //  pair<int,int>ans = pairnum(arr,size,target);
+// //   if(ans.first==-1){
+// //     cout<<"No pair found"<<endl;
+// //   }
+// //   else{
+// //      cout << "Pair found: (" << ans.first << ", " << ans.second << ")" << endl;
+// //   }
+  
+// //   return 0;
+  
+// // }
+// #include <iostream>
+// using namespace std;
+
+// void sort012(int arr[], int size)
+// {
+//     int zeroes = 0;
+//     int ones = 0;
+//     int twos = 0;
+//     for(int i = 0; i < size; i++)
+//     {
+//         if(arr[i] == 0)
+//         {
+//             zeroes++;
+//         }
+//         else if(arr[i] == 1)
+//         {
+//             ones++;
+//         }
+//         else
+//         {
+//             twos++;
+//         }
+//     }
+//     for(int i = 0; i < zeroes; i++)
+//     {
+//         arr[i] = 0;
+//     }
+//     for(int i = zeroes; i < zeroes + ones; i++)
+//     {
+//         arr[i] = 1; // Corrected line
+//     }
+//     for(int i = zeroes + ones; i < size; i++)
+//     {
+//         arr[i] = 2;
+//     }
+//     for(int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// int main()
+// {
+//     int arr[] = {0, 2, 1, 2, 0};
+//     int size = 5;
+//     sort012(arr, size);
+//     return 0;
+// }
--- swap/swapp.cpp
+++ swap/swapp.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+// #include <algorithm>
+// #include <iostream>
+// using namespace std;
 
-int main() {
-  int a=2;
-  int b=7;
-  //1)swap(a, b);
-  
-  // 2)int temp;
-  // temp=b;
-  // b=a;
-  // a=temp;
+// int main() {
+//   int a = 2;
+//   int b = 7;
+//   // 1)swap(a, b);
 
-  //3)interview artimetic
-  // a=a+b;
-  // b=a-b;
-  // a=a-b;
+//   // 2)int temp;
+//   // temp=b;
+//   // b=a;
+//   // a=temp;
 
-  //4)xor method fastest
-  a=a^b;
-   b=a^b;
-  a=a^b;
-  cout << a << " " << b << endl;
-  
-  
-  
+//   // 3)interview artimetic
+//   //  a=a+b;
+//   //  b=a-b;
+//   //  a=a-b;
 
-}
+//   // 4)xor method fastest
+//   a = a ^ b;
+//   b = a ^ b;
+//   a = a ^ b;
+//   cout << a << " " << b << endl;
+// }

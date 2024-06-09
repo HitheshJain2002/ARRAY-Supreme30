efd807b8724dede80b5544028e2ecf695d70f970
Hithesh Jain <138424080+HitheshJain2002@users.noreply.github.com>
2024-06-09 06:34:51 +0000

    ohk

---
+++ .ccls-cache/@home@runner@ARRAY-Supreme30/ROW_COL_SUM.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "enter the row_size of the array" << endl;
+    int row_size;
+    cin >> row_size;
+    cout << "enter the col_size of the array" << endl;
+
+    int col_size;
+    cin >> col_size;
+
+    vector<vector<int>> arr(row_size, vector<int>(col_size));
+
+    cout << "enter the elements of the array" << endl;
+    for(int i = 0; i < row_size; i++) {
+        for(int j = 0; j < col_size; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "output the elements of the array" << endl;
+    for(int i = 0; i < row_size; i++) {
+        for(int j = 0; j < col_size; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    int col1=0;
+    int total=0;
+    int i=0;
+    while(i<row_size) {
+        for(int j = 0; j < col_size; j++) {
+           col1 +=arr[i][j];
+           
+        }cout<<"soln of "<<i<<"th row:"<<" "<<col1<<endl;
+       total +=col1;
+        cout<<"total solution:"<<total<<endl;
+        col1=0;
+        i++;
+        cout<<endl;
+    }
+    return 0;
+}
--- array_3/vector.cpp
+++ array_3/vector.cpp
@@ -1,36 +1,36 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-int main() {
- vector<vector<int>>arr(2,vector<int>(3,0));
-  int row_size=arr.size();
-  int col_size=arr[0].size();
-  cout<<"enter the elements of the array"<<endl;
-  for(int i=0;i<row_size;i++){
-    for(int j=0;j<col_size;j++){
-      cin>>arr[i][j];
-  } cout<<endl;
-  }
-  cout<<"output the elements of the array"<<endl;
-  for(int i=0;i<row_size;i++){
-    for(int j=0;j<col_size;j++){
-      cout<<arr[i][j]<< " ";
-  }
-     cout<<endl;
-  }
-  int target=4;
-  cout<<"output the elements of the array"<<endl;
-  for(int i=0;i<row_size;i++){
-    for(int j=0;j<col_size;j++){
-      if(target==arr[i][j])
-      cout << i << "," << j;
-  }
-     cout<<endl;
-  }
+// int main() {
+//  vector<vector<int>>arr(2,vector<int>(3,0));
+//   int row_size=arr.size();
+//   int col_size=arr[0].size();
+//   cout<<"enter the elements of the array"<<endl;
+//   for(int i=0;i<row_size;i++){
+//     for(int j=0;j<col_size;j++){
+//       cin>>arr[i][j];
+//   } cout<<endl;
+//   }
+//   cout<<"output the elements of the array"<<endl;
+//   for(int i=0;i<row_size;i++){
+//     for(int j=0;j<col_size;j++){
+//       cout<<arr[i][j]<< " ";
+//   }
+//      cout<<endl;
+//   }
+//   int target=4;
+//   cout<<"output the elements of the array"<<endl;
+//   for(int i=0;i<row_size;i++){
+//     for(int j=0;j<col_size;j++){
+//       if(target==arr[i][j])
+//       cout << i << "," << j;
+//   }
+//      cout<<endl;
+//   }
   
   
-  return 0;
+//   return 0;
   
-}
+// }
